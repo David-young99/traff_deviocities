410494fc16f303b3997e24bd79be4c5faf3b41fa
unknown <davidyflol@gmail.com>
2023-07-11 12:43:21 -0600

    Alpha 2.0.0

--- Mapbox_cities.R
+++ Mapbox_cities.R
@@ -2,24 +2,62 @@
 #Mapbox is for the API facilities and settings
 #Library sf allow to edit, modify, export geospatial files, in this case a GeoJSON or Shapefile
 library(mapboxapi)
-library(googletraffic)
 library(dplyr)
 library(sf)
 
+#This sections get the day and time of the current code ejecution, this is for a custom name file save.
+current_date = Sys.Date()
+day = format(current_date, "%d")
+
+month_num = format(current_date, "%m")
+month = month.name[as.integer(month_num)]
+
+year = format(current_date, "%Y")
+
+da_te = paste0(day, "_", month)
+
 ## Set API key (This API key must be with the Mapbox account based on the GeoAdaptive google account)
 #mapbox_key <- "pk.eyJ1IjoiZHlvdW5nOTkiLCJhIjoiY2xqY3Zpaml6MjRsazNxcWcybGk3aGczdiJ9.30a_QLXc7Obxj3Ugx7lJAg"
-mapbox_key = "pk.eyJ1IjoiZGF2aWR5ZmxvbCIsImEiOiJjamdyNmRqMnAwMzBhMnhsb2oyNWx0aWk4In0.Zw_q8QejxpFOcuJJ_lWwjA"
+#mapbox_key = "pk.eyJ1IjoiZGF2aWR5ZmxvbCIsImEiOiJjamdyNmRqMnAwMzBhMnhsb2oyNWx0aWk4In0.Zw_q8QejxpFOcuJJ_lWwjA"
 
 
 ##Un/comment this path if you are running the code in windows (please change the path for your computer)
 
 #input_path = "C:\\Users\\David\\Dropbox (GeoAdaptive)\\2022_INI-04_DEVELOPMENT DASHBOARD\\DOCS\\Project development\\R code\\Traffic_DeVioCities\\traff_deviocities\\inputs\\"
-#output_path = "C:\\Users\\David\\Dropbox (GeoAdaptive)\\2022_INI-04_DEVELOPMENT DASHBOARD\\DOCS\\Project development\\R code\\Traffic_DeVioCities\\traff_deviocities\\outputs\\cities\\"
+#output_pathyear = paste0("C:\\Users\\David\\Desktop\\testcities\\", year)
+#output_pathdate = paste0(output_pathyear, "\\", da_te)
+
+#output_path = paste0("C:\\Users\\David\\Desktop\\test\\", year,"\\", da_te, "\\")
 
 ##Un/comment this path if you are running the code in VM (If you are using the GeoAdaptive GCP Instance do not change)
 
 input_path = "/home/dyoung/gitrepo/traff_deviocities/inputs/"
-output_path = "/home/dyoung/gitrepo/traff_deviocities/outputs/cities/"
+output_pathyear = paste0("/home/dyoung/gitrepo/traff_deviocities/outputs/", year)
+output_pathdate = paste0(output_pathyear, da_te)
+
+output_path = "/home/dyoung/gitrepo/traff_deviocities/outputs/"
+
+
+
+#Verifying if the year folder already exist
+
+if (dir.exists(output_pathyear)){
+  print("-")
+  print("-")
+  print("Folder Already exist, jumping creation of the year folder!")
+  print("-")
+  print("-")
+}else{
+  dir.create(output_pathyear)
+  
+  print("-")
+  print("-")
+  print("New year folder created, ¡Happy new year!")
+  print("-")
+  print("-")
+}
+
+dir.create(da_te)
 
 
 ## Grab shapefile, in this case I'm using just the Central America Shapefile
@@ -62,8 +100,6 @@ tryCatch(
   
 )
   
-  
-
-
 
 }
+
